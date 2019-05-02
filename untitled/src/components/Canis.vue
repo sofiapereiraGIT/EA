11e7c55125cc8a539b0fcb839480f5f09cfa0ec5
sofiapereiraGIT <sofiapereira9@hotmail.com>
2019-05-02 11:20:16 +0100

    Main page for all kennels

---
+++ untitled/src/components/Canis.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="w3-main" style="padding:50px;">
+        <!-- Filter Concelho -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <div class="w3-container">
+            <div class="w3-dropdown-hover" style="margin-bottom: 25px;">
+                <button class="w3-button w3-black" style="padding: 10px; width: 200px;">Concelho</button>
+                <div class="w3-dropdown-content w3-bar-block w3-border" style="width: 200px;">
+                    <a href="#" class="w3-bar-item w3-button">Link 1</a>
+                    <a href="#" class="w3-bar-item w3-button">Link 2</a>
+                    <a href="#" class="w3-bar-item w3-button">Link 3</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- First Photo Grid-->
+        <div class="w3-row-padding">
+            <div class="w3-third w3-container w3-margin-bottom">
+                <img src="../assets/FAT.jpg" style="width:100%; margin-bottom: 10px;" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <a href="#" style="text-decoration: none; color: black;"><strong>Nome</strong></a>
+                    <p>Concelho</p>
+                </div>
+            </div>
+            <div class="w3-third w3-container w3-margin-bottom">
+                <img src="../assets/FAT.jpg" style="width:100%" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <p><b>Nome</b></p>
+                    <p>Concelho</p>
+                </div>
+            </div>
+            <div class="w3-third w3-container">
+                <img src="../assets/FAT.jpg" style="width:100%" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <p><b>Nome</b></p>
+                    <p>Concelho</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Second Photo Grid-->
+        <div class="w3-row-padding">
+            <div class="w3-third w3-container w3-margin-bottom">
+                <img src="../assets/FAT.jpg" style="width:100%" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <p><b>Nome</b></p>
+                    <p>Concelho</p>
+                </div>
+            </div>
+            <div class="w3-third w3-container w3-margin-bottom">
+                <img src="../assets/FAT.jpg" style="width:100%" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <p><b>Nome</b></p>
+                    <p>Concelho</p>
+                </div>
+            </div>
+            <div class="w3-third w3-container">
+                <img src="../assets/FAT.jpg" style="width:100%" class="w3-hover-opacity">
+                <div class="w3-container w3-white">
+                    <p><b>Nome</b></p>
+                    <p>Concelho</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+/* todo: usar uma imagem predefinida qd o canil não tem imagem */
+export default {
+}
+</script>
+
+<style scoped>
+</style>
