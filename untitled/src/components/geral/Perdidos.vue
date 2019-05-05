e8d6243679f9d7e1d822347a0371537852d77712
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2019-05-05 17:49:55 +0100

    criadas páginas de perfil dus utilizadores e canis

--- untitled/src/components/geral/Perdidos.vue
+++ untitled/src/components/geral/Perdidos.vue
@@ -5,16 +5,18 @@
         </div>
         <br>
         <div class="w3-row">
-            <div class="w3-col m6">
+            <div class="w3-container w3-col l3 m1 w3-hide-small"></div>
+            <div class="w3-col l2 m4">
                 <a href="">
                     <img border="0" alt="W3Schools" src="../../assets/cao.png" width="350" height="350">
                 </a>
             </div>
-            <div class="w3-col m6">
+            <div class="w3-col m5">
                 <a href="">
                     <img border="0" alt="W3Schools" src="../../assets/gato.png" width="780" height="450">
                 </a>
             </div>
+            <div class="w3-container w3-col l3 m1 w3-hide-small"></div>
         </div>
     </div>
 </template>
---
+++ untitled/src/components/geral/UserPerfil.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="w3-row w3-padding-32 w3-section">
+            <div class="w3-col m6 w3-container">
+                <img src="/w3images/map.jpg" class="w3-image w3-round" style="width:100%"><br>
+                <br>
+                <a href="url">Veja os cães desta entidade</a><br>
+                <a href="url">Veja os gatos desta entidade</a>
+            </div>
+            <div class="w3-col m4 w3-panel">
+                <div class="w3-large w3-margin-bottom">
+                    <i class="fas fa-user fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com nome<br>
+                    <i class="fas fa-info-circle fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com descrição .................<br>
+                    <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com email<br>
+                    <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com tlm<br>
+                    <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com concelho<br>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPerfil'
+}
+</script>
+
+<style scoped>
+
+</style>
