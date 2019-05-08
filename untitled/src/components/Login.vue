c450179fbdbcef3c18de104071e41ac1cebdba97
cpp <a77320@alunos.uminho.pt>
2019-05-08 10:37:23 +0100

    login

--- untitled/src/components/Login.vue
+++ untitled/src/components/Login.vue
@@ -1,13 +1,30 @@
 <template>
-
+  <div class="Background">
+    <div class="LoginPopUp">
+        <h1>LOGIN FORM</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Login'
-  }
 </script>
 
-<style scoped>
+<style>
+.Background {
+  background-image: url('../assets/homepage.jpg');
+  height: 1366px;
+  width: 1920px;
+}
 
+.LoginPopUp {
+  border-radius: 25px;
+  background: #73AD21;
+  position: absolute;
+  width:500px;
+  height:650px;
+  left:50%;
+  top:50%;
+  margin-left:-390px;
+  margin-top:-325px;
+}
 </style>
