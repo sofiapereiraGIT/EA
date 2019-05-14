3f8edfc0d7f826ac842fb03525b2cfd5e0abcf17
cpp <a77320@alunos.uminho.pt>
2019-05-14 10:43:26 +0100

    Interface para Adicionar o Animal para Adoção e para Gerir os Animais Adicionadas

---
+++ untitled/src/components/AddAnimalParaAdocao.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="background">
+    <div class="transbox">
+      <h1>Adicionar Animal</h1>
+      <form>
+        <input type="text" name="animalName" value="Nome" required>
+        <br>
+        <br>
+        <input type="file" name="picture" accept="image/*" required>
+        <br>
+        <br>
+        <select style="width: 25%" required>
+          <option selected disabled>Tipo</option>
+          <option value="1">Cão</option>
+          <option value="2">Gato</option>
+        </select>
+        <select style="width: 25%" required>
+          <option selected disabled>Sexo</option>
+          <option value="1">Macho</option>
+          <option value="2">Fêmea</option>
+        </select>
+        <br>
+        <br>
+        <select style="width: 25%" required>
+          <option selected disabled>Cor do Pêlo</option>
+          <option value="1">Amarelo</option>
+          <option value="2">Bege</option>
+          <option value="3">Branco</option>
+          <option value="4">Castanho</option>
+          <option value="5">Cinzento</option>
+          <option value="6">Preto</option>
+          <option value="7">Indefinido</option>
+        </select>
+        <select style="width: 25%" required>
+          <option selected disabled>Idade</option>
+          <option value="1">Bebé (Menos de 6 Meses)</option>
+          <option value="2">Jovem</option>
+          <option value="3">Adulto</option>
+        </select>
+        <br>
+        <br>
+        <select style="width: 25%" required>
+          <option selected disabled>Tipo de Pêlo</option>
+        </select>
+        <select style="width: 25%" required>
+          <option selected disabled>Porte</option>
+          <option value="1">Pequeno</option>
+          <option value="2">Médio</option>
+          <option value="3">Grande</option>
+        </select>
+        <br>
+        <br>
+        <select style="width: 25%" required>
+          <option selected disabled>Comprimento de Pêlo</option>
+          <option value="1">Sem Pêlo</option>
+          <option value="2">Curto</option>
+          <option value="3">Médio</option>
+          <option value="4">Longo</option>
+        </select>
+        <select style="width: 25%" required>
+          <option selected disabled>Raça Cão</option>
+          <option value="1">Pastor Alem̃ao</option>
+          <option value="2">Lavrador</option>
+          <option value="3">Buldogue</option>
+          <option value="4">Beagle</option>
+          <option value="5">Poodle</option>
+          <option value="6">Rottwiller</option>
+          <option value="7">Golden Retriever</option>
+          <option value="8">Outro</option>
+        </select>
+        <br>
+        <br>
+        <select style="width: 50%" required>
+          <option selected disabled>Raça Gato</option>
+          <option value="1">Persa</option>
+          <option value="2">Siamês</option>
+          <option value="3">Ragdoll</option>
+          <option value="4">Scottish Fold</option>
+          <option value="5">Outro</option>
+        </select>
+        <br>
+        <br>
+        <input type="text" name="descricao" value="Descrição">
+        <br>
+        <br>
+        <select style="width: 50%" required>
+          <option selected disabled>Estado</option>
+          <option value="1">Pedido FAT</option>
+          <option value="2">Em FAT</option>
+          <option value="3">Encontrado</option>
+          <option value="4">Adotado</option>
+        </select>
+        <br>
+        <br>
+        <button class="button">Registar</button>
+        <button class="button">Cancelar</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .background {
+    width: 100%;
+    height: 100vh;
+    margin-top: 38px;
+    border: 1px solid black;
+    background: url("../assets/cao2.jpg");
+  }
+  .transbox {
+    opacity: 0.7;
+    margin: 10% 25%;
+    text-align: center;
+    border-radius: 10px;
+    background-color: #fff;
+    border: 3px solid black;
+  }
+  .transbox p {
+    margin: 5%;
+    font-weight: bold;
+    color: black;
+  }
+  input[type=text] {
+    width: 50%;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 12px;
+    background-color: white;
+    padding: 10px;
+  }
+  .button {
+    border: none;
+    color: white;
+    float: right;
+    cursor: pointer;
+    font-size: 16px;
+    margin-right: 1%;
+    border-radius: 10%;
+    padding: 15px 25px;
+    text-align: center;
+    display: inline-block;
+    background-color: black;
+    transition-duration: 0.4s;
+    -webkit-transition-duration: 0.4s;
+  }
+  .button:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+  }
+</style>
---
+++ untitled/src/components/GerirAnimais.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="row">
+    <div class="LeftMenuWithBlackBG">
+      <p style="font-size: 20px">Filtros</p>
+      <hr>
+      <form>
+        <label>
+          <select style="width: 100%">
+            <option value="0">Tipo: Todos</option>
+            <option value="1">Cão</option>
+            <option value="2">Gato</option>
+          </select>
+        </label>
+        <br>
+        <br>
+        <label>
+          <select style="width: 100%">
+            <option value="0">Estado: Todos</option>
+            <option value="1">Adotado</option>
+            <option value="2">Não Adotado</option>
+            <option value="3">Encontrado</option>
+            <option value="4">Falecido</option>
+            <option value="5">Em FAT</option>
+            <option value="6">Em Pedido de FAT</option>
+            <option value="7">Sem Pedido de FAT</option>
+          </select>
+        </label>
+      </form>
+    </div>
+    <div class="rightMenu">
+      <label>
+        <input type="text" name="search" placeholder="Nome do Animal">
+      </label>
+      <br>
+      <br>
+      <div class="animalCol">
+        <img src="../assets/cat2.jpg" alt="Snow" style="width:100%">
+      </div>
+      <div class="animalCol">
+        <img src="../assets/cat2.jpg" alt="Forest" style="width:100%">
+      </div>
+      <div class="animalCol">
+        <img src="../assets/cat2.jpg" alt="Mountains" style="width:100%">
+      </div>
+      <div class="animalCol">
+        <img src="../assets/cat2.jpg" alt="Mountains" style="width:100%">
+      </div>
+    </div>
+    <button class="button">Adicionar Animal</button>
+    <button class="button">Comunicar Desaparecimento</button>
+    <button class="button" style="float: left; margin-left: 1%">Voltar</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .row {
+    margin-top: 38px;
+  }
+  .LeftMenuWithBlackBG {
+    width: 20%;
+    float: left;
+    color: white;
+    height: 100vh;
+    background-color: black;
+    text-align: center;
+  }
+  .rightMenu {
+    float: left;
+    width: 80%;
+    padding: 5px;
+    text-align: center;
+  }
+  .animalCol {
+    float: left;
+    width: 25%;
+    padding: 10px;
+  }
+  input[type=text] {
+    width: 20%;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 12px;
+    background-color: white;
+    padding: 10px;
+  }
+  .button {
+    border: none;
+    color: white;
+    float: right;
+    cursor: pointer;
+    font-size: 16px;
+    margin-right: 1%;
+    border-radius: 10%;
+    padding: 15px 25px;
+    text-align: center;
+    display: inline-block;
+    text-decoration: none;
+    background-color: black;
+    transition-duration: 0.4s;
+    -webkit-transition-duration: 0.4s;
+  }
+  .button:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+  }
+</style>
