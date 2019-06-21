cfa855c932d0019a74dd64a1d87111a3afbde04c
David Sousa <davidsousa@MBP-de-David-2.lan>
2019-06-21 12:25:40 +0100

    UserPerfil atualizado

--- untitled/src/components/geral/UserPerfil.vue
+++ untitled/src/components/geral/UserPerfil.vue
@@ -10,27 +10,27 @@
             <div class="w3-col m4 w3-panel">
                 <div class="w3-row-padding w3-container" style="clear: both; float: left">
                     <div class="w3-large w3-margin-bottom">
-                        <i class="fas fa-user fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>preencher com nome<br>
+                        <i class="fas fa-user fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>{{UserInfo.nome}}<br>
                     </div>
                 </div>
                 <div class="w3-row-padding w3-container" style="clear: both; float: left">
                     <div class="w3-large w3-margin-bottom">
-                        <i class="fas fa-info-circle fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com descrição .................<br>
+                        <i class="fas fa-info-circle fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>{{UserInfo.descricao}}<br>
                     </div>
                 </div>
                 <div class="w3-row-padding w3-container" style="clear: both; float: left">
                     <div class="w3-large w3-margin-bottom">
-                        <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com email<br>
+                        <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>{{UserInfo.email}}<br>
                     </div>
                 </div>
                 <div class="w3-row-padding w3-container" style="clear: both; float: left">
                     <div class="w3-large w3-margin-bottom">
-                        <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com tlm<br>
+                        <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>{{UserInfo.telemovel}}<br>
                     </div>
                 </div>
                 <div class="w3-row-padding w3-container" style="clear: both; float: left">
                     <div class="w3-large w3-margin-bottom">
-                        <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> preencher com concelho<br>
+                        <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>{{UserInfo.concelho}}<br>
                     </div>
                 </div>
             </div>
@@ -39,9 +39,25 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-  name: 'UserPerfil'
+  name: 'UserPerfil',
+  data: () => ({
+    UserInfo: {}
+  }),
+
+  mounted: function () {
+    axios.defaults.headers['Content-Type'] = 'application/json'
+    axios.get('http://localhost:8080/procura4patas//UtilizadorComum?emailPedido=' + this.$session.get('user')[0] + '&email=' + this.$session.get('user')[0])
+      .then(response => {
+        this.UserInfo = response.data
+        console.log(this.UserInfo)
+      }).catch()
+  }
 }
+
 </script>
 
 <style scoped>
