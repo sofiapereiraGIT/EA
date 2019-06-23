a30ea0041b4feee19acc7511b3a0b5d321dfb266
sofiapereiraGIT <sofiapereira9@hotmail.com>
2019-06-23 04:13:24 +0100

    Pagination and canis re-use

--- untitled/src/components/geral/Canis.vue
+++ untitled/src/components/geral/Canis.vue
@@ -12,7 +12,7 @@
                 <div class="w3-dropdown-content w3-bar-block w3-border" style="width: 200px;">
                     <option class="w3-bar-item w3-button"
                             v-for="concelho in concelhos" :key="concelho"
-                            v-on:click="selConcelho=concelho">
+                            v-on:click="selConcelho=concelho, page=0">
                         {{concelho}}
                     </option>
                     <option class="w3-bar-item w3-button" v-on:click="selConcelho=''">
@@ -25,7 +25,7 @@
         <!-- First Photo Grid-->
         <div class="w3-row-padding" style="margin-top: 25px;">
             <div class="w3-quarter w3-container w3-margin-bottom"
-                 v-for="canil in canisDoSelconcelho" :key="canil.email">
+                 v-for="canil in canisDoSelconcelho[page]" :key="canil.email">
                 <router-link to="/CanilPerfil">
                     <img
                         v-if="canil.fotografia==='' || canil.fotografia===null"
@@ -46,6 +46,34 @@
                 </div>
             </div>
         </div>
+
+        <!-- Pagination -->
+        <nav class="w3-center" v-if="canisDoSelconcelho.length>8" style="margin-bottom: 25px;">
+            <div class="w3-bar">
+                <button class="w3-button" v-bind:disabled="page===0" v-on:click="page -= 1">
+                    Anterior
+                </button>
+                <button class="w3-button"
+                        v-if="page<Math.floor(canisDoSelconcelho.length/nrPerPage) && p+page>=1"
+                        v-bind:class="{ 'w3-green':  page  === p+page-1 }"
+                        v-for="p in [0,1,2]" :key="p+page"
+                        v-on:click="page = p+page-1">
+                    {{p+page}}
+                </button>
+                <button class="w3-button"
+                        v-if="page===Math.floor(canisDoSelconcelho.length/nrPerPage)"
+                        v-bind:class="{ 'w3-green':  page  === Math.floor(canisDoSelconcelho.length/nrPerPage)+p-1 }"
+                        v-for="p in [-1,0,1]" :key="Math.floor(canisDoSelconcelho.length/nrPerPage)+p"
+                        v-on:click="page = Math.floor(canisDoSelconcelho.length/nrPerPage)-p-1">
+                    {{Math.floor(canisDoSelconcelho.length/nrPerPage)+p}}
+                </button>
+                <button class="w3-button"
+                        v-bind:disabled="page===Math.floor(canisDoSelconcelho.length/nrPerPage)"
+                        v-on:click="page += 1">
+                    Próximo
+                </button>
+            </div>
+        </nav>
     </div>
 </template>
 
@@ -57,39 +85,60 @@ export default {
 
   data: () => ({
     canis: [],
+    page: 0,
+    nrPerPage: 12,
     selCanilEmail: '',
     selConcelho: ''
   }),
 
   mounted: function () {
-    if (this.$session.has('user')) {
-      axios.defaults.headers['Content-Type'] = 'application/json'
-      axios
-        .get('http://localhost:8080/procura4patas/Canis?email=' + this.$session.get('user')[0])
-        .then(response => {
-          this.canis = response.data
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    if (!this.$session.has('canis') || this.$session.get('canis')[1] > 15) {
+      console.log('getCanis')
+
+      if (this.$session.has('user')) {
+        axios.defaults.headers['Content-Type'] = 'application/json'
+        axios
+          .get('http://localhost:8080/procura4patas/Canis?email=' + this.$session.get('user')[0])
+          .then(response => {
+            this.canis = response.data
+
+            function compare (a, b) {
+              if (a.nome < b.nome) { return -1 }
+              if (a.nome > b.nome) { return 1 }
+              return 0
+            }
+
+            this.canis.sort(compare)
+            this.$session.set('canis', [this.canis, 1])
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        axios.defaults.headers['Content-Type'] = 'application/json'
+        axios
+          .get('http://localhost:8080/procura4patas/Canis')
+          .then(response => {
+            this.canis = response.data
+
+            function compare (a, b) {
+              if (a.nome < b.nome) { return -1 }
+              if (a.nome > b.nome) { return 1 }
+              return 0
+            }
+
+            this.canis.sort(compare)
+            this.$session.set('canis', [this.canis, 1])
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
     } else {
-      axios.defaults.headers['Content-Type'] = 'application/json'
-      axios
-        .get('http://localhost:8080/procura4patas/Canis')
-        .then(response => {
-          this.canis = response.data
-
-          function compare (a, b) {
-            if (a.nome < b.nome) { return -1 }
-            if (a.nome > b.nome) { return 1 }
-            return 0
-          }
-
-          this.canis.sort(compare)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+      this.canis = this.$session.get('canis')[0]
+      var times = this.$session.get('canis')[1] + 1
+      this.$session.set('canis', [this.canis, times])
+      console.log('reutilizar canis da sessão pela ' + times + ' vez')
     }
   },
 
@@ -118,7 +167,28 @@ export default {
         }
       })
 
-      return canisDoSelconcelho
+      var i = 0
+      var p = 0
+      var page = []
+      page[p] = []
+      var nrPerPage = this.nrPerPage
+
+      canisDoSelconcelho.forEach(function (c) {
+        if (i < nrPerPage) {
+          page[p].push(c)
+          page.push(c)
+          i++
+        } else {
+          p++
+          page[p] = []
+          i = 0
+          i++
+          page[p].push(c)
+          page.push(c)
+        }
+      })
+
+      return page
     }
   }
 }
