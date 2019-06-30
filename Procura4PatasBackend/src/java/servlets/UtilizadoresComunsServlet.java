1c5f20518d4d89a19e5ac5247496dcc337d2298b
sofiapereiraGIT <sofiapereira9@hotmail.com>
2019-06-30 10:58:48 +0100

    Servlet for all users.

---
+++ Procura4PatasBackend/src/java/servlets/UtilizadoresComunsServlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.orm.PersistentSession;
+import procura4patas.UtilizadorComum;
+import src.P4P;
+import src.Util;
+
+/**
+ *
+ * @author sofia
+ */
+@WebServlet(name = "UtilizadoresComunsServlet", urlPatterns = {"/UtilizadoresComuns"})
+public class UtilizadoresComunsServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+    
+        PersistentSession session;
+        String email = request.getParameter("email");
+    
+        if(email  ==  null) {
+            session = Util.getSessionWithoutAut(request);
+        } else {
+            session = Util.getSession(request, email);
+        }
+
+        List<UtilizadorComum> utilizadoresComuns = P4P.getUtilizadoresComuns(session);
+        JSONArray array = new JSONArray();
+
+        for(UtilizadorComum c: utilizadoresComuns){
+            JSONObject result = new JSONObject();           
+            result.put("email", c.getEmail());
+            result.put("password", c.getPassword());
+            result.put("nome", c.getNome());
+            result.put("fotografia", c.getFotografia());
+            result.put("concelho", c.getConcelho());
+            result.put("telemovel", c.getTelemovel());
+            result.put("descricao", c.getDescricao());
+            array.add(result);
+        } 
+
+        PrintWriter out = response.getWriter();
+           out.println(array);
+           out.flush();
+           out.close();
+    }
+    
+    @Override
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {   
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        
+        PrintWriter out = response.getWriter();
+        out.flush();
+        out.close();
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
